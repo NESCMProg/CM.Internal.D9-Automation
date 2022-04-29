7522263f06a892fae51c0693008c9b93100957a8
cchitneedi <100114810+cchitneedi@users.noreply.github.com>
2022-04-29 13:16:02 +0530

    Update HomePage.java
    import org.openqa.selenium.support.ui.WebDriverWait; is added

--- D9-Automation/src/main/java/pageObjects/HomePage.java
+++ D9-Automation/src/main/java/pageObjects/HomePage.java
@@ -3,7 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
 import resources.base;
 
 public class HomePage extends base{
